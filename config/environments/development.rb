4b2b18a165ed36ea90c9d55ce356d73af03643ee
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2022-02-01 20:17:02 +0100

    isk

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
+  # Enable/disable caching. By default caching is disabled.  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
