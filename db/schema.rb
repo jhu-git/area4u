704cf2f3e5dfcf9f2072f8e05b4a071bb9ad7048
Gina Slager <g.natasia@live.nl>
2022-02-10 19:47:21 +0100

    geocoded by location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_190348) do
+ActiveRecord::Schema.define(version: 2022_02_10_183324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2022_02_09_190348) do
     t.boolean "availability"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
