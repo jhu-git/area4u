41b00027b139deeb62561b0f2cfae2d609838f1f
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2022-02-11 23:06:53 +0100

    book properties, property page, availability search

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,7 +4,13 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @ooking = Booking.new
+    @booking = Booking.new(booking_params)
+    if available?
+      @booking.save
+      redirect_to dashboard_path
+    else
+      redirect_to property_path(params[:property_id]), info: "The property is booked for those dates/times."
+    end
   end
 
   def new
@@ -21,5 +27,31 @@ class BookingsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def booking_params
+    {
+      user_id: params[:user_id],
+      property_id: params[:property_id],
+      start: params[:start],
+      end: params[:end],
+      guests: params[:guests]
+    }
+  end
+
+  def available?
+    bookings = Booking.where(property_id: params[:property_id])
+    booleans = []
+    bookings.each do |booking|
+      booleans << (booking[:start] <= params[:start] && booking[:end] <= params[:start])
+      booleans << (booking[:start] >= params[:start] && booking[:start] >= params[:end])
+      booleans << (booking[:start] <= params[:end] && booking[:end] <= params[:start])
+      booleans << (booking[:start] >= params[:end] || booking[:end] <= params[:start])
+      booleans << (booking[:end] >= params[:start] && booking[:start] >= params[:end])
+      booleans << (booking[:end] <= params[:end] && booking[:end] >= params[:start])
+    end
+    return !booleans.include?(false)
   end
 end
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,21 +2,34 @@
 class PropertiesController < ApplicationController
   def index
 
-    if params[:query].present?
+    if params[:query].present? || params[:check_in].present? || params[:check_out].present?
       sql_query = "name ILIKE :query
                    OR location ILIKE :query
                    OR category ILIKE :query
                    OR theme ILIKE :query"
-      @properties = Property.where(sql_query, query: "%#{params[:query]}%")
+      @allproperties = Property.where(sql_query, query: "%#{params[:query]}%")
+      # @properties = @all_properties.select do |property|
+      #   available?(property)
+      #   raise
+      # end
+      @properties = @allproperties.reject{|property| !available?(property)}
     else
       @properties = Property.all
     end
-    
-    @markers = @properties.geocoded.map do |property|
-      {
-        lat: property.latitude,
-        lng: property.longitude
-      }
+    if @properties.class != Array
+      @markers = @properties.geocoded.map do |property|
+        {
+          lat: property.latitude,
+          lng: property.longitude
+        }
+      end
+    else
+      @markers = @allproperties.geocoded.map do |property|
+        {
+          lat: property.latitude,
+          lng: property.longitude
+        }
+      end
     end
   end
 
@@ -37,6 +50,33 @@ class PropertiesController < ApplicationController
   end
 
   def update
+  end
+
+  def show
+    if user_signed_in?
+      @property = Property.find(params[:id])
+      @booking = Booking.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  private
+
+  def available?(property)
+    bookings = Booking.where(property_id: property)
+    booleans = []
+    bookings.each do |booking|
+      if booking[:start] != nil && booking[:end] != nil
+        booleans << (booking[:start] <= params["check_in"] && booking[:end] <= params["check_in"])
+        booleans << (booking[:start] >= params["check_in"] && booking[:start] >= params["check_out"])
+        booleans << (booking[:start] <= params["check_out"] && booking[:end] <= params["check_in"])
+        booleans << (booking[:start] >= params["check_out"] || booking[:end] <= params["check_in"])
+        booleans << (booking[:end] >= params["check_in"] && booking[:start] >= params["check_out"])
+        booleans << (booking[:end] <= params["check_out"] && booking[:end] >= params["check_in"])
+      end
+    end
+    return !booleans.include?(false)
   end
 
 end
